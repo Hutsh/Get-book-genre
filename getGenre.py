ea6e334fdcf06b34121e336b82031d346e84fa67
Tianshuo Hu <hxbbun@gmail.com>
2018-04-12 11:04:14 -0400

    Add files via upload

--- getGenre.py
+++ getGenre.py
@@ -1,45 +1,60 @@
 import re
 import requests
+import random
+import string
+import time
 
-def getGenre():
+def start():
     dataPath = r'E:\PITT\2018Spring\Algorithm\Project\books2.txt'
     outpath = r'E:\PITT\2018Spring\Algorithm\Project\booksout.txt'
     out = open(outpath, 'w')
-    out.write('Hello, world!')
-    webpath = r'https://www.alibris.com/booksearch?keyword='
-    pattern = re.compile(r'red\/home.gif.+?<li>.+?<li>\s+?<a.+?>(.+?)<\/a>')
-    psub1 = re.compile(r'red.+">')
-    psub2 = re.compile(r'</a>')
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
-        'From': 'youremail@domain.com'  # This is another valid field
-    }
 
     with open(dataPath, 'r') as f:
         lines = f.readlines()
         for line in lines:
             l = line.split("\t")
             isbn = l[4]
-            print("ISBN: "+isbn)
-            p = webpath+isbn+r'&mtype=B'
-            h = requests.get(p,headers=headers)
-            t = h.text
-
-            match = pattern.search(t)
-            if match:
-                match = match.group()
-                m = psub1.sub('',match)
-                m = psub2.sub('',m)
-
-                outs = isbn+'\t'+m
-                print(outs)
-                out.write(outs)
-            else:
-                outs = isbn + '\t' + "ERROR"
-                print(outs)
-                out.write(outs)
-
+            genre = getGenre(isbn)
+            outs = isbn+'\t'+genre+'\n'
+            print(outs)
+            out.write(outs)
     out.close()
 
+def getGenre(ISBN):
+    urlpre = r'https://www.alibris.com/booksearch?keyword='
+    pattern = re.compile(r'red\/home.gif.+?<li>.+?<li>\s+?<a.+?>(.+?)<\/a>')
+    psub1 = re.compile(r'red.+">')
+    psub2 = re.compile(r'</a>')
+    highvolpattern = re.compile(r'due to the high volume of visitors')
+    headers = {
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+        'From': ''  # This is another valid field
+    }
+
+    headersfrom = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5))
+    headers['From'] = headersfrom + "@alibris.com"
+    url = urlpre + ISBN + r'&mtype=B'
+    html = requests.get(url, headers=headers)
+    text = html.text
+
+    match = pattern.search(text)
+    if match:
+        m = match.group()
+        m = psub1.sub('', m)
+        m = psub2.sub('', m)
+        return m
+    else:
+        highvol = highvolpattern.search(text)
+        if highvol:
+            print('wait 1 second\n')
+            time.sleep(1)
+            getGenre(ISBN)
+        else:
+            return 'NA'
+
+
 if __name__ == "__main__":
-    getGenre()
+    start()
+    #print(getGenre('9780140283330'))
+
+
