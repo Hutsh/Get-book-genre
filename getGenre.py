bba2c527b1125997fd06274a26c897d162a5b3e3
Tianshuo Hu <hxbbun@gmail.com>
2018-04-12 14:03:13 -0400

    random change UA

--- getGenre.py
+++ getGenre.py
@@ -4,6 +4,9 @@ import random
 import string
 import time
 
+waittimes = 0
+
+
 def start():
     dataPath = r'E:\PITT\2018Spring\Algorithm\Project\books2.txt'
     outpath = r'E:\PITT\2018Spring\Algorithm\Project\booksout.txt'
@@ -12,10 +15,11 @@ def start():
     with open(dataPath, 'r') as f:
         lines = f.readlines()
         for line in lines:
+            line = line.strip('\n')
             l = line.split("\t")
             isbn = l[4]
             genre = getGenre(isbn)
-            outs = isbn+'\t'+genre+'\n'
+            outs = line+'\t'+genre+'\n'
             print(outs)
             out.write(outs)
     out.close()
@@ -27,9 +31,23 @@ def getGenre(ISBN):
     psub2 = re.compile(r'</a>')
     highvolpattern = re.compile(r'due to the high volume of visitors')
     headers = {
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+        'User-Agent': 'Mozilla/5.0 (compatible; WOW64; MSIE 10.0; Windows NT 6.2)',
         'From': ''  # This is another valid field
     }
+    UA = ['Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+          'Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.9 Safari/537.36',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36 OPR/48.0.2685.52',
+          'Mozilla/5.0 (Windows NT 10.0; WOW64; rv:56.0) Gecko/20100101 Firefox/56.0',
+          'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US) AppleWebKit/533.20.25 (KHTML, like Gecko) Version/5.0.4 Safari/533.20.27',
+          'Mozilla/5.0 (Windows NT 10.0; WOW64; Trident/7.0; rv:11.0) like Gecko',
+          'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36 Edge/15.15063',
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36',
+          'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.49 Safari/537.36 OPR/48.0.2685.7',
+          'Mozilla/5.0 (X11; Linux i586; rv:31.0) Gecko/20100101 Firefox/31.0',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/62.0.3202.75 Safari/537.36',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.78 Safari/537.36 OPR/47.0.2631.55',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.12; rv:56.0) Gecko/20100101 Firefox/56.0',
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_6) AppleWebKit/604.3.5 (KHTML, like Gecko) Version/11.0.1 Safari/604.3.5']
 
     headersfrom = ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(5))
     headers['From'] = headersfrom + "@alibris.com"
@@ -38,18 +56,32 @@ def getGenre(ISBN):
     text = html.text
 
     match = pattern.search(text)
+    global waittimes
     if match:
         m = match.group()
         m = psub1.sub('', m)
         m = psub2.sub('', m)
+        waittimes = 0
         return m
     else:
         highvol = highvolpattern.search(text)
+
         if highvol:
-            print('wait 1 second\n')
+            if waittimes >= 3:
+                print('long wait... INNER PEACE...')
+                for s in range(1, 60):
+                    print(s)
+                    time.sleep(1)
+                waittimes = 0
+            print('wait')
             time.sleep(1)
-            getGenre(ISBN)
+            waittimes += 1
+            print('wait tiems = '+ str(waittimes) + '\n')
+            headers['User-Agent'] = UA[random.randint(0, 13)]
+            res = getGenre(ISBN)
+            return res
         else:
+            waittimes = 0
             return 'NA'
 
 
